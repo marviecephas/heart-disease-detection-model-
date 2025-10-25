b281da0548c9afd028702d1dba68b307a25c1f00
marvellous <120428022+marviecephas@users.noreply.github.com>
2025-10-25 02:03:09 +0100

    Update main.py
    + logistic_regression + confusion_matrix

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-importude = 'number')
+from google.colab import drive
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+ sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+
+drive.mount('/content/drive')
+file_path = '/content/drive/MyDrive/heart.csv'
+
+df = pd.read_csv(file_path)
+print(df.head(50))
+print(df.info())
+print(df.describe())
+
+numerical_df = df.select_dtypes(include = 'number')
 categorical_df = df.select_dtypes(include = 'object')
 
 plt.figure(figsize = (10, 8))
@@ -18,6 +35,18 @@ for col in categorical_df.columns :
 
 plt.show()
 
+df['Cholesterol'] = df['Cholesterol'].replace(0, np.nan)
+df['Cholesterol'].fillna(df['Cholesterol'].median(), inplace = True)
+
+plt.figure(figsize = (10, 8))
+sns.histplot(data = df, x = 'Cholesterol', hue = 'HeartDisease')
+
+plt.show()
+
+
+df = pd.get_dummies(df, columns = categorical_df.columns, drop_first = True)
+
+
 X = df.drop(columns = ['HeartDisease'])
 y = df['HeartDisease']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
@@ -36,4 +65,4 @@ precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 
-print(f'accuracy = {accuracy} \n precision = {precision} \n recall = {recall} \n f1 = {f1_score}')
+print(f'accuracy = {accuracy} \n precision = {precision} \n recall = {recall} \n f1 = {f1}')
