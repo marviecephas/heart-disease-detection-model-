fa812ebafbb0d7a774ba1d58496608f040d284c6
marvellous <120428022+marviecephas@users.noreply.github.com>
2025-10-25 02:01:05 +0100

    Update main.py
    + logisti_ regression + confusion_matrix

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd # Import pandas
-
-file_path = 'heart.csv'
-
-df = pd.read_csv(file_path)
-print(df.head(50))
-print(df.info())
-print(df.describe())
-
-numerical_df = df.select_dtypes(include = 'number')
+importude = 'number')
 categorical_df = df.select_dtypes(include = 'object')
 
 plt.figure(figsize = (10, 8))
@@ -29,3 +17,23 @@ for col in categorical_df.columns :
   sns.histplot(data = df, x = col, hue = 'HeartDisease')
 
 plt.show()
+
+X = df.drop(columns = ['HeartDisease'])
+y = df['HeartDisease']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+logr = LogisticRegression()
+logr.fit(X_train, y_train)
+y_pred = logr.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+cmd = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = [0,1])
+cmd.plot(cmap = 'Blues')
+plt.show()
+
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print(f'accuracy = {accuracy} \n precision = {precision} \n recall = {recall} \n f1 = {f1_score}')
